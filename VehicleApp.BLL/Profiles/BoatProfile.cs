2d386fb89d0141811304170b6054a7a8733040f3
Bahadir Erdogan <bahadirerdogan@outlook.com.tr>
2022-11-16 15:45:34 +0300

    Dto's created and then mapped to entites with using AutoMapper.

---
+++ VehicleApp.BLL/Profiles/BoatProfile.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VehicleApp.Core.Entities.Concrete;
+using VehicleApp.Dtos.Boats;
+
+namespace VehicleApp.BLL.Profiles
+{
+    public class BoatProfile : Profile
+    {
+        public BoatProfile()
+        {
+            CreateMap<Boat, BoatDto>();
+        }
+    }
+}
